d1adae33ec64c3b7b5b8c9d0f6dceb48b75a4f29
senorbeast <44683244+senorbeast@users.noreply.github.com>
2022-10-08 16:14:02 +0530

    fix: :bug: Fix linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,26 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"prettier"
-	],
-	"overrides": [
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"plugins": [
-		"@typescript-eslint",
-		"prettier"
-	],
-	"rules": {
-		"prettier/prettier": ["error"],
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+    indent: "off",
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,8 @@
 
 module.exports = {
   trailingComma: "es5",
-  tabWidth: 4,
+  tabWidth: 2,
+  useTabs: false,
   semi: true,
   singleQuote: false,
 };
